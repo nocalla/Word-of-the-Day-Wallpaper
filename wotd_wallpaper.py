31de403501a7bcee5b96f56b822c510eff53d18d
nocalla <niallwocallaghan@gmail.com>
2018-02-18 15:09:18 +0000

    Improved HTML parsing Fix #1

--- wotd_wallpaper.py
+++ wotd_wallpaper.py
@@ -13,12 +13,69 @@ import requests
 import ctypes
 import textwrap
 
+from html.parser import HTMLParser
+
 
 wotd_link = "http://www.dictionary.com/wordoftheday"
-wotd_filter = "Definitions for <strong>(.*)<\/strong>\s*         .*\s*.*\s*.*<span>(.*)\."
+wotd_filter = "Definitions for (\w*)\s(.*)"
 fonts = ["Penshurs.ttf", "georgia.ttf"]
 base = "base_wallpaper.png"
 output_filename = "wotd_wallpaper.png"
+
+
+class _HTMLToText(HTMLParser):
+    # from https://gist.github.com/Crazometer/af441bc7dc7353d41390a59f20f07b51
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self._buf = []
+        self.hide_output = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag in ('p', 'br') and not self.hide_output:
+            self._buf.append('\n')
+        elif tag in ('script', 'style'):
+            self.hide_output = True
+
+    def handle_startendtag(self, tag, attrs):
+        if tag == 'br':
+            self._buf.append('\n')
+
+    def handle_endtag(self, tag):
+        if tag == 'p':
+            self._buf.append('\n')
+        elif tag in ('script', 'style'):
+            self.hide_output = False
+
+    def handle_data(self, text):
+        if text and not self.hide_output:
+            self._buf.append(re.sub(r'\s+', ' ', text))
+
+    def handle_entityref(self, name):
+        if name in name2codepoint and not self.hide_output:
+            c = chr(name2codepoint[name])
+            self._buf.append(c)
+
+    def handle_charref(self, name):
+        if not self.hide_output:
+            n = int(name[1:], 16) if name.startswith('x') else int(name)
+            self._buf.append(chr(n))
+
+    def get_text(self):
+        return re.sub(r' +', ' ', ''.join(self._buf))
+
+
+def html_to_text(html):
+    """
+    Given a piece of HTML, return the plain text it contains.
+    This handles entities and char refs, but not javascript and stylesheets.
+    """
+    parser = _HTMLToText()
+    try:
+        parser.feed(html)
+        parser.close()
+    except:  #HTMLParseError: No good replacement?
+        pass
+    return parser.get_text()
 
 
 def get_wotd():
@@ -26,14 +83,13 @@ def get_wotd():
     res = requests.get(wotd_link)
     res.raise_for_status()
     regex = re.compile(wotd_filter)
-    match = regex.search(res.text)
+    text = html_to_text(res.text)
+    match = regex.search(text)
+    
     if match:
         word = match.group(1).capitalize().strip()
         definition = match.group(2).strip()
-        definition = definition.replace("<em>", "")
-        definition = definition.replace("</em>", "")
-        definition = definition.replace("&rsquo;", "'")
-
+        print("\t{}:\n\t{}".format(word, definition))
         return [word, definition]
 
 
@@ -90,5 +146,6 @@ def set_wallpaper(file):
 
 img = Image.open(base)
 draw = ImageDraw.Draw(img)
-bg = print_wotd(get_wotd())
-set_wallpaper(bg)
+get_wotd() # debug
+#bg = print_wotd(get_wotd())
+#set_wallpaper(bg)
