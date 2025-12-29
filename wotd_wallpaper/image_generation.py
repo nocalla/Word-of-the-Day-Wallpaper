6c795124c4ad8d7d6d8e53a1c62ac9e442728251
Niall O'Callaghan <nocalla@users.noreply.github.com>
2025-12-29 19:11:58 +0000

    Rework image generation to use dataclasses

--- wotd_wallpaper/image_generation.py
+++ wotd_wallpaper/image_generation.py
@@ -1,11 +1,13 @@
 import configparser
 import os
 import textwrap
+from dataclasses import dataclass, field, fields
 
+from get_wotd import WordOfTheDay
 from PIL import Image, ImageDraw, ImageFont
 
 
-def get_configs():
+def get_configs() -> configparser.ConfigParser:
     """Retrieve all configuration parameters."""
     conf_files = ["wotd_wallpaper.conf", "user_configuration.conf"]
     if not os.path.exists("wotd_wallpaper.conf"):
@@ -15,125 +17,139 @@ def get_configs():
     return config
 
 
-class WallpaperImage:
-    def __init__(self, wotd: dict, output_filename: str):
-        """
-        :param  wotd: list of data [word, definition, pronunciation]
-        :param  img:
-        :param  config:
-        :param  output_filename:
-        """
-        self.img = Image.open("base_wallpaper.png")
-        self.config = get_configs()
-        self.wotd_dict = wotd
-        self.output_filename = output_filename
+def generate_image(
+    wotd: WordOfTheDay,
+    output_filename: str,
+    base_image_path: str,
+    config: configparser.ConfigParser,
+) -> str:
+    """
+    generate wallpaper image with word of the day text overlaid
+    :param wotd: WordOfTheDay dataclass object with word data
+    :param output_filename: filename to save generated image as
+    :param base_image_path: path to base image to overlay text on
+    :return: path to generated image
+    """
+    img = Image.open(base_image_path)
+    # offset each section by height of previous text box
+    current_offset = 0
+    # add to image for each section in wotd object
+    for parameter in fields(wotd):
+        Format = FieldFormat(config=config, section=parameter.name)
+        current_offset = write_msg(
+            img,
+            msg=getattr(wotd, parameter.name),
+            current_offset=current_offset,
+            Format=Format,
+        )
+    img.save(output_filename)
+    return output_filename
 
-    def run(self) -> str:
-        """
-        create output image with text overlaid on background
-        :return self.output_filename: filename of our image
-        """
-        wotd_dict = self.wotd_dict
-        # only change the file if there's something to change it to
-        if wotd_dict:
-            # offset each section by height of previous text box
-            current_offset = 0
-            # add to image for each section in wotd dictionary
-            for key in wotd_dict.keys():
-                current_offset = self.write_msg(
-                    wotd_dict[key], key, current_offset
-                )
-            self.img.save(self.output_filename)
-        return self.output_filename
 
-    def write_msg(
-        self, msg: str, conf_section: str, current_offset: float
-    ) -> float:
-        """
-        write a line of text on the image according to specified parameters
-        :param msg: text to write
-        :param conf_section: string name of section in config object to use
-        :param font: font to use
-        :param font_size: font size
-        :param h_offset: horizontal offset of text box from centre of image
-        :param v_offset: vertical offset of text box from centre of image
-        :return [w, h]: width and height of text box
-        """
-        font = self.config.get(conf_section, "Font")
-        font_size = self.get_conf_int(conf_section, "Size")
-        h_offset = self.get_conf_int(conf_section, "Horizontal offset")
-        v_offset = self.get_conf_int(conf_section, "Vertical offset")
-        colour = self.fix_colour_string(
-            self.config.get(conf_section, "Colour")
+@dataclass
+class FieldFormat:
+    config: configparser.ConfigParser
+    section: str = ""
+    font: str = ""
+    font_size: int = field(default_factory=int)
+    h_offset: int = field(default_factory=int)
+    v_offset: int = field(default_factory=int)
+    colour: tuple[int, int, int] = field(default_factory=tuple)
+
+    def __post_init__(self) -> None:
+        self.font = self.config.get(self.section, "Font")
+        self.font_size = get_conf_int(self.config, self.section, "Size")
+        self.h_offset = get_conf_int(
+            self.config, self.section, "Horizontal offset"
+        )
+        self.v_offset = get_conf_int(
+            self.config, self.section, "Vertical offset"
+        )
+        self.colour = fix_colour_string(
+            self.config.get(self.section, "Colour")
         )
 
-        if font_size > 0:
-            font_obj = ImageFont.truetype(font, font_size)
-            W, H = self.img.size
-            text_bb = font_obj.getbbox(msg)
-            w = text_bb[2] - text_bb[0]
-            h = text_bb[3] - text_bb[1]
-            ascent, descent = font_obj.getmetrics()
-            h = ascent + descent + v_offset
 
-            if current_offset == 0:
-                current_offset = v_offset + ((H - h) / 2)
+def write_msg(
+    img, msg: str, Format: FieldFormat, current_offset: float
+) -> float:
+    """
+    write a line of text on the image according to specified parameters
+    :param msg: text to write
+    :param Format: FieldFormat object with formatting parameters
+    :param current_offset: current vertical offset from top of image
+    :return [w, h]: width and height of text box
+    """
 
-            # wrap string if it's too long
-            if w >= (0.95 * W):
-                self.wrap_string(msg, conf_section, current_offset)
-                return 0
-            pos = (((W - w) / 2) + h_offset, current_offset)
+    if Format.font_size > 0:
+        font_obj = ImageFont.truetype(Format.font, Format.font_size)
+        W, H = img.size
+        text_bb = font_obj.getbbox(msg)
+        w = text_bb[2] - text_bb[0]
+        h = text_bb[3] - text_bb[1]
+        ascent, descent = font_obj.getmetrics()
+        h = ascent + descent + Format.v_offset
 
-            draw = ImageDraw.Draw(self.img)
-            draw.text(pos, msg, colour, font_obj)
-            # draw.rectangle(
-            #     [pos, (pos[0] + w, pos[1] + h)],
-            #     fill=None,
-            #     outline=(255, 255, 255),
-            # )  # debug
-            current_offset += h
+        if current_offset == 0:
+            current_offset = Format.v_offset + ((H - h) / 2)
 
-        return current_offset
+        # wrap string if it's too long
+        if w >= (0.95 * W):
+            wrap_string(img, msg, Format, current_offset)
+            return 0
+        pos = (((W - w) / 2) + Format.h_offset, current_offset)
 
-    def get_conf_int(self, section, param):
-        """
-        get integer from named config section for named param
-        :param  section: section name in self.config
-        :param  param: parameter in self.config
-        :return integer: integer corresponding to param entry
-        """
-        integer = 0
-        str_param = self.config.get(section, param)
-        if str_param.replace(" ", "") != "":
-            integer = self.config.getint(section, param)
-        return integer
+        draw = ImageDraw.Draw(img)
+        draw.text(pos, msg, Format.colour, font_obj)
+        # draw.rectangle(
+        #     [pos, (pos[0] + w, pos[1] + h)],
+        #     fill=None,
+        #     outline=(255, 255, 255),
+        # )  # debug
+        current_offset += h
 
-    def fix_colour_string(self, str):
-        """
-        converts string of "(255, 255, 255)" into tuple of same
-        :param str: string version of tuple
-        :return col: usable colour tuple
-        """
-        str = str.replace("(", "")
-        str = str.replace(")", "")
-        str = str.replace(" ", "")
-        string_list = str.split(",")
-        col = (int(string_list[0]), int(string_list[1]), int(string_list[2]))
-        return col
+    return current_offset
 
-    def wrap_string(self, msg, conf_section, current_offset):
-        """
-        split message into lines and wrap text if it's too wide
-        :param msg: text to write
-        :param font: font to use
-        :param font_size: font size
-        :param h_offset: horizontal offset of text box from centre of image
-        :param v_offset: vertical offset of text box from centre of image
-        """
-        wrapped_list = textwrap.wrap(msg, 100)
-        line_space = 60
-        for index, line in enumerate(wrapped_list):
-            v = (index * line_space) + current_offset
-            self.write_msg(line, conf_section, v)
-        return
+
+def get_conf_int(config, section, param) -> int:
+    """
+    get integer from named config section for named param
+    :param  section: section name in config
+    :param  param: parameter in config
+    :return integer: integer corresponding to param entry
+    """
+    integer = 0
+    str_param = config.get(section, param)
+    if str_param.replace(" ", "") != "":
+        integer = config.getint(section, param)
+    return integer
+
+
+def fix_colour_string(str) -> tuple[int, int, int]:
+    """
+    converts string of "(255, 255, 255)" into tuple of same
+    :param str: string version of tuple
+    :return col: usable colour tuple
+    """
+    str = str.replace("(", "")
+    str = str.replace(")", "")
+    str = str.replace(" ", "")
+    string_list = str.split(",")
+    col = (int(string_list[0]), int(string_list[1]), int(string_list[2]))
+    return col
+
+
+def wrap_string(img, msg: str, Format: FieldFormat, current_offset) -> None:
+    """
+    split message into lines and wrap text if it's too wide
+    :param msg: text to write
+    :param Format: FieldFormat object with formatting parameters
+    :param current_offset: current vertical offset from top of image
+    :return: None
+    """
+    wrapped_list = textwrap.wrap(msg, 100)
+    line_space = 60
+    for index, line in enumerate(wrapped_list):
+        v = (index * line_space) + current_offset
+        write_msg(img, line, Format, v)
+    return
